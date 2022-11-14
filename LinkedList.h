db07841d65be03b57c5c24e18c96c705b4508dae
Kamel R <kamel.r@wayne.edu>
2022-11-14 18:16:03 -0500

    Add project files.

---
+++ LinkedList.h
@@ -0,0 +1,43 @@
+#pragma once
+
+
+template <class Type>
+struct nodeType
+{
+	Type info; // data 
+
+	nodeType<Type>* link; // pointer to the next node
+
+};
+
+template <class Type>
+class LinkedList
+{
+private: 
+	nodeType<Type>* first; 
+	int count=0; // how many elements we have 
+
+public:
+	int getCount();
+
+	bool insertAt(Type data, int index);
+
+	void insertAtEnd(Type data);
+
+	void insertAtStart(Type data); 
+
+	bool deleteAt(int index);
+
+	bool deleteLast();
+
+	bool deleteFirst(); 
+
+	bool deleteNode(Type data); 
+
+	bool exists(Type data);
+
+	
+	
+
+};
+
